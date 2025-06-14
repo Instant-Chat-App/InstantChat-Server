3b5bddf9714c6b2272bfc3689c44da4a5d4568a1
toanehihi <hocongtoan987@gmail.com>
2025-06-15 00:07:27 +0700

    test chat controller

---
+++ src/controllers/chat.controller.ts
@@ -0,0 +1,21 @@
+import { ChatService } from "../services/chat.service";
+
+
+export default class ChatController {
+    private chatService: ChatService;
+
+    constructor(chatService: ChatService) {
+        this.chatService = chatService;
+    }
+
+    async getUserChats(req: any, res: any) {
+        try {
+            const userId = Number(req.params.userId);
+            const chats = await this.chatService.getUserChats(userId);
+            res.status(200).json(chats);
+        } catch (error) {
+            console.error("Error fetching user chats:", error);
+            res.status(500).json({ message: "Internal server error" });
+        }
+    }
+}
