4ffbd61a0e66f91a08522f55f912c18f04301bb3
toanehihi <hocongtoan987@gmail.com>
2025-06-15 17:51:05 +0700

    chore

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ import { create } from 'domain';
 import { createServer } from 'http';
 import { getEnv } from './utils/get-env.service';
 import routerConfig from './config/router.config';
-import getSocketIO from './socketio/socket-io';
 import { logger } from './utils/logger';
 import corsMiddleware from './config/cors.config';
+import setupSocket from './socketio/socket-io';
 
 
 initializeApplication();
@@ -20,11 +20,12 @@ async function initializeApplication(){
   const app = express();
   const server = createServer(app);
 
+  setupSocket(server);
+
   const port = getEnv('PORT', '8080');
   app.use(corsMiddleware);
   app.use("/api", routerConfig);
-
-  getSocketIO(server);  
+  
   server.listen(port, () => {
     logger.info(`Server is running at http://localhost:${port}`);
   });
