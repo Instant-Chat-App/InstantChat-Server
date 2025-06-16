24e7a5b369111d8e9a62af3e9f52b90697035ef0
Lezh1n <hoangphilong1208@gmail.com>
2025-06-16 12:46:52 +0700

    update routers

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import AuthController from "../controllers/auth.controller";
+import { validate } from "../middlewares/validate.middleware";
+import { registerSchema } from "../schemas/auth.schema";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const authRouter = Router();
 
 const authController = new AuthController();
 
-authRouter.post("/login", (req, res) => {
-  authController.login(req, res);
+authRouter.post("/login", validate(registerSchema), (req, res) => {
+  authController.login(req, res, );
 });
 
-authRouter.post("/register", (req, res) => {
+authRouter.post("/register", validate(registerSchema), (req, res) => {
   authController.register(req, res);
 });
 
@@ -19,6 +22,10 @@ authRouter.post("/refresh", (req, res) => {
 
 authRouter.post("/logout", (req, res) => {
   authController.logout(req, res);
+});
+
+authRouter.get("/profile", authMiddleware, (req, res) => {
+  authController.getProfile(req, res);
 });
 
 export default authRouter;
