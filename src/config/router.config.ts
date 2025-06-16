44c499fb2f7df136bfcc0df452060f8c42726752
Lezh1n <hoangphilong1208@gmail.com>
2025-06-16 09:08:40 +0700

    chore

--- src/config/router.config.ts
+++ src/config/router.config.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import chatRouter from "../routers/chat.router";
 import messageRouter from "../routers/message.router";
+import authRouter from "../routers/auth.router";
 const routerConfig = Router();
 
 routerConfig.use("/chats", chatRouter);
 routerConfig.use("/messages", messageRouter);
+routerConfig.use("/auth", authRouter);
 
 export default routerConfig;
