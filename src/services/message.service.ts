f53b74247dd1a2bdd5ded665f0c2198ba17a04dc
toanehihi <hocongtoan987@gmail.com>
2025-06-16 14:52:46 +0700

    fixing

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -6,7 +6,13 @@ export class MessageService{
         this.messageRepository = new MessageRepository();
     }
 
+    // Retrieves messages for a specific user in a chat
     async getUserChatMessages(userId: number, chatId: number) {
         return await this.messageRepository.getUserChatMessages(userId, chatId);
     }
+    
+    async sendMessage(userId: number, chatId: number, content: string, attachments: any[] = []) {
+        return null;
+    }
+    
 }
