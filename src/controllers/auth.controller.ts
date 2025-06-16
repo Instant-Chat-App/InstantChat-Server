cace2e89490b3d9a080456b684f58f012c37090d
Lezh1n <hoangphilong1208@gmail.com>
2025-06-16 12:38:13 +0700

    refactor

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,99 +1,64 @@
-import { LoginRequest } from "../dtos/login-request.dto";
+import { LoginRequest } from "../dtos/requests/login-request.dto";
 import { Request, Response } from "express";
 import { AuthService } from "../services/auth.service";
 import { logger } from "../utils/logger";
-import { RegisterRequest } from "../dtos/register-request.dto";
+import { RegisterRequest } from "../dtos/requests/register-request.dto";
+import { DataResponse } from "../dtos/responses/DataResponse";
+import "../middlewares/auth.middleware";
 
-export class AuthController {
-  private readonly authService = new AuthService();
+export default class AuthController {
+  private readonly authService: AuthService;
 
   constructor() {
     this.authService = new AuthService();
   }
 
-  login = async (req: Request, res: Response): Promise<void> => {
+  async login(req: Request, res: Response) {
     try {
       const loginRequest: LoginRequest = req.body;
       const result = await this.authService.login(loginRequest);
 
-      if (!result) {
-        res.status(401).json({
-          success: false,
-          message: "Invalid credentials",
-        });
-        return;
+      if (result instanceof DataResponse) {
+        return res.status(result.code).json(result);
       }
 
-      res.status(200).json({
-        success: true,
-        message: "Login successful",
-        data: result,
-      });
-    } catch (error) {
+      res.status(200).json(DataResponse.success(result, "Login successfully"));
+    } catch (error: any) {
       logger.error(`Login error: ${error}`);
-      res.status(500).json({
-        success: false,
-        message: "Internal server error",
-      });
+      res
+        .status(500)
+        .json(DataResponse.error("Internal server error", error.message));
     }
-  };
+  }
 
-  register = async (req: Request, res: Response): Promise<void> => {
+  async register(req: Request, res: Response) {
     try {
+      console.log("Request body:", req.body);
       const registerRequest: RegisterRequest = req.body;
-      // Validate input
-      if (!registerRequest.phoneNumber || !registerRequest.password) {
-        res.status(400).json({
-          success: false,
-          message: "Phone number, password are required",
-        });
-        return;
-      }
 
       const result = await this.authService.register(registerRequest);
 
-      if (!result) {
-        res.status(409).json({
-          success: false,
-          message: "Phone number already exists",
-        });
-        return;
+      if (result instanceof DataResponse) {
+        return res.status(result.code).json(result);
       }
-
-      res.status(201).json({
-        success: true,
-        message: "Registration successful",
-        data: result,
-      });
-    } catch (error) {
+      res
+        .status(201)
+        .json(DataResponse.success(result, "Registration successfully"));
+    } catch (error: any) {
       logger.error(`Registration error: ${error}`);
-      res.status(500).json({
-        success: false,
-        message: "Internal server error",
-      });
+      res
+        .status(500)
+        .json(DataResponse.error("Internal server error", error.message));
     }
-  };
+  }
 
-  refresh = async (req: Request, res: Response): Promise<void> => {
+  async refresh(req: Request, res: Response) {
     try {
       const { refreshToken } = req.body;
 
-      if (!refreshToken) {
-        res.status(400).json({
-          success: false,
-          message: "Refresh token is required",
-        });
-        return;
-      }
-
       const tokens = this.authService.refreshToken(refreshToken);
-
-      if (!tokens) {
-        res.status(401).json({
-          success: false,
-          message: "Invalid or expired refresh token",
-        });
-        return;
+      if (tokens instanceof DataResponse) {
+        return res.status(tokens.code).json(tokens);
       }
 
       res.status(200).json({
@@ -101,46 +66,59 @@ export class AuthController {
         message: "Token refreshed successfully",
         data: tokens,
       });
-    } catch (error) {
+    } catch (error: any) {
       logger.error(`Token refresh error: ${error}`);
-      res.status(500).json({
-        success: false,
-        message: "Internal server error",
-      });
+      res
+        .status(500)
+        .json(DataResponse.error("Internal server error", error.message));
     }
-  };
+  }
 
-  logout = async (req: Request, res: Response): Promise<void> => {
+  async logout(req: Request, res: Response) {
     try {
       const { refreshToken } = req.body;
 
-      if (!refreshToken) {
-        res.status(400).json({
-          success: false,
-          message: "Refresh token is required",
-        });
-        return;
-      }
-
       const result = this.authService.logout(refreshToken);
 
-      if (await result) {
-        res.status(200).json({
-          success: true,
-          message: "Logged out successfully",
-        });
-      } else {
-        res.status(400).json({
-          success: false,
-          message: "Invalid refresh token",
-        });
+      if (result instanceof DataResponse) {
+        return res.status(result.code).json(result);
       }
-    } catch (error) {
+
+      res
+        .status(200)
+        .json(DataResponse.success(result, "Log out successfully"));
+    } catch (error: any) {
       logger.error(`Logout error: ${error}`);
-      res.status(500).json({
-        success: false,
-        message: "Internal server error",
-      });
+      res
+        .status(500)
+        .json(DataResponse.error("Internal server error", error.message));
     }
-  };
+  }
+
+  async getProfile(req: Request, res: Response) {
+    try {
+      const user = req.user;
+
+      if (!user) {
+        return res
+          .status(401)
+          .json(DataResponse.badRequest("User authentication required"));
+      }
+
+      const result = await this.authService.getProfile(user.accountId);
+
+      if (result instanceof DataResponse) {
+        return res.status(result.code).json(result);
+      }
+
+      res
+        .status(200)
+        .json(DataResponse.success(result, "Get profile successfully"));
+    } catch (error: any) {
+      logger.error(`Get profile error: ${error}`);
+      res
+        .status(500)
+        .json(DataResponse.error("Internal server error", error.message));
+    }
+  }
 }
