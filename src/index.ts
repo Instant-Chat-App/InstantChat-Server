2ae5e3ebaaed1c1aa81936de42a7d8f6c8ca2442
Lezh1n <hoangphilong1208@gmail.com>
2025-06-17 13:37:32 +0700

    cors config

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { createServer } from "http";
 import { getEnv } from "./utils/get-env.service";
 import routerConfig from "./config/router.config";
 import { logger } from "./utils/logger";
-import corsMiddleware from "./config/cors.config";
 import setupSocket from "./socketio/socket-io";
+import corsConfig from "./config/cors.config";
 
 initializeApplication();
 
@@ -13,6 +13,7 @@ async function initializeApplication() {
   await initApplication();
 
   const app = express();
+  app.use(corsConfig);
   const server = createServer(app);
 
   app.use(express.json());
@@ -21,7 +22,6 @@ async function initializeApplication() {
   const port = getEnv("PORT", "8080");
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(corsMiddleware);
   app.use("/api", routerConfig);
 
   server.listen(port, () => {
