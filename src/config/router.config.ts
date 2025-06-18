1f304e3ecabfbc9e927f6c3374c638512518dbe9
Lezh1n <hoangphilong1208@gmail.com>
2025-06-18 15:41:46 +0700

    update router

--- src/config/router.config.ts
+++ src/config/router.config.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import chatRouter from "../routers/chat.router";
 import messageRouter from "../routers/message.router";
 import authRouter from "../routers/auth.router";
+import userRouter from "../routers/user.router";
 const routerConfig = Router();
 
 routerConfig.use("/chats", chatRouter);
@@ -10,5 +11,6 @@ routerConfig.use("/messages", messageRouter);
 
 routerConfig.use("/auth", authRouter);
 
+routerConfig.use("/users", userRouter);
 
 export default routerConfig;
---
+++ src/routers/user.router.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import UserController from "../controllers/user.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+
+const userRouter = Router();
+const userController = new UserController();
+
+userRouter.get("/contacts", authMiddleware, (req, res) => {
+  userController.getUserContacts(req, res);
+});
+
+userRouter.get("/:userId", authMiddleware, (req, res) => {
+  userController.getUserInfo(req, res);
+});
+
+export default userRouter;
